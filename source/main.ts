49391e634a79fe9cac4877edc3b9c48306ddcc30
Kaique Silva <kaikzyx@gmail.com>
2025-05-11 19:05:44 -0300

    feat(main): set up sketch gallery with p5.js

---
+++ source/main.ts
@@ -0,0 +1,18 @@
+import "./style.css";
+import p5 from "p5";
+
+// Get the sketch name from the URL path.
+const SKETCH = window.location.pathname.slice(1);
+
+if (SKETCH) {
+    import(`./sketches/${SKETCH}/index.ts`)
+        .then((module) => {
+            document.title = `sketch: ${SKETCH}`;
+            new p5(module.default, document.getElementById("sketch")!);
+        })
+        .catch((_error) => {
+            document.body.innerHTML = `<h1>Sketch "${SKETCH}" not found.</h1>`;
+        });
+} else {
+    document.body.innerHTML = `<h1>Please add a route to load a sketch.</h1>`;
+}
